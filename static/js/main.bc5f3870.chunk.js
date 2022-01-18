3e74cc3edc1f156b86f63f3b3c10583c7d470c27
EogoK <egorakfeil@gmail.com>
2022-01-18 22:12:33 +0500

    Updates

---
+++ static/js/main.bc5f3870.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmini_apps_rassilka=this.webpackJsonpmini_apps_rassilka||[]).push([[0],{130:function(n,e,s){"use strict";s.r(e);s(0);var t=s(7),i=s.n(t),a=s(2),p=s.n(a),r=s(10),c=(s(128),s(4)),o=function(){return Object(c.jsx)(r.a,{children:"Hell"})};p.a.send("VKWebAppInit"),i.a.render(Object(c.jsx)(o,{}),document.getElementById("root"))}},[[130,1,2]]]);
+//# sourceMappingURL=main.bc5f3870.chunk.js.map
